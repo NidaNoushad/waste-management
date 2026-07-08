656f1971ab606ae649c86c4b1a178ecbd8154276
Nida <nida11noushad@gmail.com>
2026-07-08 23:53:18 +0530

    feat: connect to Supabase PostgreSQL

--- waste_management/waste_management/settings.py
+++ waste_management/waste_management/settings.py
@@ -107,13 +107,14 @@ WSGI_APPLICATION = 'waste_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
